fa2773cce9bd199c9fec28d8f916f54fe2ba2506
PashaBalandin <pashabalandin1200@gmail.com>
2021-05-23 21:10:20 +0300

    User Authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from "axios";
 
 Vue.use(VueRouter)
 
@@ -24,50 +25,50 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Home.vue')
     },
     {
         path: '/search',
         name: 'search',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Search.vue')
     },
     {
         path: '/playlists',
         name: 'playlists',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Playlists.vue')
     },
     {
         path: '/artists',
         name: 'artists',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Artists.vue')
 
     },
     {
         path: '/genres',
         name: 'genres',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Genres.vue')
     },
     {
         path: '/album',
         name: 'album',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/single/Album.vue')
     },
     {
         path: '/artist',
         name: 'artist',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/single/Artist.vue')
     },
     {
         path: '/playlist',
         name: 'playlist',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/single/Playlist.vue')
     }
 
@@ -79,4 +80,34 @@ const router = new VueRouter({
     routes
 })
 
+router.beforeEach((to, from, next) => {
+    const requiredAuth = to.matched.some(record => record.meta.auth)
+    check().then((data) => {
+        console.log(requiredAuth)
+        console.log(data)
+        if (requiredAuth && data) {
+            next('/login')
+        } else {
+            next()
+        }
+    });
+
+})
+
 export default router
+
+async function check() {
+    try {
+        const result = await axios.get('http://localhost:8080/api/v1/tags', {
+            headers: {Authorization: 'Bearer_' + localStorage.getItem('token')}
+        });
+        console.log(result.status)
+        if (result.status === 200) {
+            return false;
+        } else {
+            return true;
+        }
+    } catch (e) {
+        return true;
+    }
+}
